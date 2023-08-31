ae1ffb70ac41a48b62cbf97afb0cb109c3784ae5
Felipe <lipe.ferreira2002@gmail.com>
2023-08-31 19:01:52 -0300

    linkando categorias da aba lateral da main page para redirecionar pra página da categoria

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: "https://rica.onrender.com",
-    // NEXT_PUBLIC_API_URL: 'http://127.0.0.1:3333',
+    // NEXT_PUBLIC_API_URL: "https://rica.onrender.com",
+    NEXT_PUBLIC_API_URL: 'http://127.0.0.1:3333',
   },
 };
 
--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,60 +1,61 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import { Typography } from '@mui/material';
-import { Category } from '@/data/@types/category';
-import { Line } from '../Line';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import { Typography } from "@mui/material";
+import { Category } from "@/data/@types/category";
+import { Line } from "../Line";
+import Link from "next/link";
 
 interface CategoriasListProps {
-    categories: Category[];
+  categories: Category[];
 }
 
 const CategoriasList: React.FC<CategoriasListProps> = ({ categories }) => {
-    return (
-        <Box sx={{ width: '100%', }}>
-            <Line color="#000000" />
-            <List
-                component='nav'
-                subheader={
-                    <Typography
-                        component="div"
-                        fontFamily="'Poppins', sans-serif"
-                        sx={{
-                            fontWeight: '600',
-                            fontSize: '18px',
-                            lineHeight: '2',
-                            background: 'transparent',
-                            verticalAlign: 'baseline',
-                            m: '1rem 0 1rem 0'
-                        }}
-                    >
-                        Categorias
-                    </Typography>
-                }
-            >
-                {categories.map((categoria) => (
-                    <ListItem disablePadding>
-                        <ListItemButton sx={{ pl: 0 }}>
-                            <ListItemText
-                                primary={categoria.name}
-                                sx={{
-                                    "span": {
-                                        fontFamily: "'Poppins', sans-serif"
-                                    }
-                                }}
-                            />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-
-        </Box>
-    );
-}
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Line color="#000000" />
+      <List
+        component="nav"
+        subheader={
+          <Typography
+            component="div"
+            fontFamily="'Poppins', sans-serif"
+            sx={{
+              fontWeight: "600",
+              fontSize: "18px",
+              lineHeight: "2",
+              background: "transparent",
+              verticalAlign: "baseline",
+              m: "1rem 0 1rem 0"
+            }}
+          >
+            Categorias
+          </Typography>
+        }
+      >
+        {categories.map((categoria) => (
+          <ListItem disablePadding>
+            <ListItemButton sx={{ pl: 0 }}>
+              <Link href={`/articlesbycategory/${categoria.id}`}>
+                <ListItemText
+                  primary={categoria.name}
+                  sx={{
+                    span: {
+                      fontFamily: "'Poppins', sans-serif"
+                    }
+                  }}
+                />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+};
 
 export default CategoriasList;
-
