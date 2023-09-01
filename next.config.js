c5b1795433778759ae225cc8be435ea0d217d42b
Felipe <lipe.ferreira2002@gmail.com>
2023-09-01 14:56:38 -0300

    .

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     // NEXT_PUBLIC_API_URL: "https://rica.onrender.com",
-    NEXT_PUBLIC_API_URL: 'http://127.0.0.1:3333',
+    NEXT_PUBLIC_API_URL: 'http://localhost:3333',
   },
 };
 
